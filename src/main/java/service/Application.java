1c815936467685e597171098e6e65a8529ffa605
Yuantao <yin0823@gmail.com>
2015-04-21 11:53:35 -0400

    Add camel

--- src/main/java/service/Application.java
+++ src/main/java/service/Application.java
@@ -1,10 +1,12 @@
 package service;
 
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.servlet.CamelHttpTransportServlet;
 import org.apache.camel.spring.SpringCamelContext;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.embedded.ServletRegistrationBean;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
@@ -14,7 +16,9 @@ import org.springframework.context.annotation.Bean;
 public class Application extends SpringBootServletInitializer {
 
 	private static Class<Application> appClass = Application.class;
-	
+	private static final String CAMEL_URL_MAPPING = "/camel/*";
+    private static final String CAMEL_SERVLET_NAME = "CamelServlet";
+    
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
@@ -24,15 +28,23 @@ public class Application extends SpringBootServletInitializer {
         return application.sources(appClass);
     }
     
-//  @Bean
-//  public SpringCamelContext camelContext(ApplicationContext applicationContext) throws Exception {
-//	  SpringCamelContext camelContext = new SpringCamelContext(applicationContext);
-//	  camelContext.addRoutes(routeBuilder());
-//	  return camelContext;
-//  }
-//  
-//  @Bean
-//  public RouteBuilder routeBuilder() {
-//	  return new SampleRoute();
-//  }
+    @Bean
+    public ServletRegistrationBean servletRegistrationBean() {
+        ServletRegistrationBean registration = new ServletRegistrationBean(new CamelHttpTransportServlet(), CAMEL_URL_MAPPING);
+        registration.setName(CAMEL_SERVLET_NAME);
+        return registration;
+    }
+
+    @Bean
+    public SpringCamelContext camelContext(ApplicationContext applicationContext) throws Exception {
+        SpringCamelContext camelContext = new SpringCamelContext(applicationContext);
+        camelContext.addRoutes(routeBuilder());
+        return camelContext;
+    }
+
+    @Bean
+    public RouteBuilder routeBuilder() {
+        return new MyRouteBuilder();
+    }
+    
 }
